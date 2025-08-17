ad60e0805ce0a0033e1362e8b25d29abcd6ff13e
wesley Souza <wesleysilvanfl@gmail.com>
2025-08-17 16:24:48 -0300

    feat: implement authentication with JWT and integrate WebSocket support for messaging

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,20 @@
+import { forwardRef, Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { AuthService } from './auth.service';
+import { JwtStrategy } from './jwt.strategy';
+import { UserModule } from '../user/user.module';
+
+@Module({
+  imports: [
+    forwardRef(() => UserModule),
+    PassportModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET || 'segredo',
+      signOptions: { expiresIn: '1d' },
+    }),
+  ],
+  providers: [AuthService, JwtStrategy],
+  exports: [AuthService],
+})
+export class AuthModule {}
